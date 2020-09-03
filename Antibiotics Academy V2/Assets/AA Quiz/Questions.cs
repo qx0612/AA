12681e0b20bea5e5608c5787cc544930b6dad357
qx0612 <66994919+qx0612@users.noreply.github.com>
2020-09-03 14:03:13 +0800

    aa

--- Antibiotics Academy V2/Assets/AA Quiz/Questions.cs
+++ Antibiotics Academy V2/Assets/AA Quiz/Questions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Questions
+public class Questions         //a questions class that stores the question, question number and its answer
 {
     public string questions;
     public int questionNo;
--- Antibiotics Academy V2/Assets/AA Quiz/QuizManager.cs
+++ Antibiotics Academy V2/Assets/AA Quiz/QuizManager.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class QuizManager : MonoBehaviour
 {
-    List<Questions> qns = new List<Questions>();
+    List<Questions> qns = new List<Questions>(); //a list to store the questions
 
-    public string nextScene;
+    public string nextScene;                  
 
-    public int sceneID;
+    public int sceneID; 
 
     public TextAsset questionsCSV;
 
@@ -24,46 +24,46 @@ public class QuizManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string[] data = questionsCSV.text.Split(new char[] { '\n' });
+        string[] data = questionsCSV.text.Split(new char[] { '\n' });     //a string array that stores the split items from the csv
 
-        for (int i = 1; i < data.Length - 1; i++)
+        for (int i = 1; i < data.Length - 1; i++)                         //for loop that stores the data into its respective variables
         {
-            string[] row = data[i].Split(new char[] { ',' });
-            Questions question = new Questions();
-            int.TryParse(row[0], out question.questionNo);
-            question.questions = row[1];
-            question.answer = row[2].Trim();
+            string[] row = data[i].Split(new char[] { ',' });             //splits the data on the ,
+            Questions question = new Questions();                         //create a new Question instance
+            int.TryParse(row[0], out question.questionNo);                //parse through the integer
+            question.questions = row[1];                                  //set the questions from the string in row 1  
+            question.answer = row[2].Trim();                              //store the answer from the string in row 2
 
-            qns.Add(question);
+            qns.Add(question);                                            //add the question instance to the questions list
         }
 
-        questionNo.text = "Question: " + qns[index].questionNo;
+        questionNo.text = "Question: " + qns[index].questionNo;           //set the questionNo text and question text to the first question, since index starts at 0
         question.text = qns[index].questions;
     }
 
-    public void TrueBtn()
+    public void TrueBtn()        //function to set the ans to true
     {
-        ans = "TRUE";
+        ans = "TRUE";            
         CheckAnswer();
     }
 
-    public void FalseBtn()
+    public void FalseBtn()       //function to set the ans to false
     {
         ans = "FALSE";
         CheckAnswer();
     }
 
-    void CheckAnswer()
+    void CheckAnswer()           //function to check answer
     {
-        if (index < qns.Count && index != qns.Count - 1)
+        if (index < qns.Count && index != qns.Count - 1)    //if the current qns is less than the total question
         {
-            if (ans == qns[index].answer)
+            if (ans == qns[index].answer)                   //if answer is the same as the answer in the question instance
             {
-                score++;
+                score++;                                    //increase score
                 Debug.Log("added");
             }
-            index++;
-            NextQuestion();
+            index++;                                        //increase index
+            NextQuestion();                                 
         }
 
         else
@@ -85,11 +85,11 @@ public class QuizManager : MonoBehaviour
 
     }
 
-    void NextQuestion()
+    void NextQuestion()         //function to display next question
     {
-        if (index < qns.Count)
+        if (index < qns.Count)  //if index is less than questions count
         {
-            questionNo.text = "Question: " + qns[index].questionNo;
+            questionNo.text = "Question: " + qns[index].questionNo;  //set the questionNo text and question text based on the index
             question.text = qns[index].questions;
         }
     }
