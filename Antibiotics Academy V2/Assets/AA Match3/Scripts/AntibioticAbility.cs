616d65070f20c4e964748b0be21719f47c44a3b7
Mxchyxo <70682131+Mxchyxo@users.noreply.github.com>
2020-09-03 13:07:35 +0800

    Update AntibioticAbility.cs

--- Antibiotics Academy V2/Assets/AA Match3/Scripts/AntibioticAbility.cs
+++ Antibiotics Academy V2/Assets/AA Match3/Scripts/AntibioticAbility.cs
@@ -20,34 +20,34 @@ namespace Match3
 
         private void Update()
         {
-            if (healthManager.healthState == HealthStates.Sick && counter > 0)
+            if (healthManager.healthState == HealthStates.Sick && counter > 0)        //if the health state is in sick and the counter is greater than 0
             {
-                btn.interactable = true;
+                btn.interactable = true;                                              //then the antibiotic ability button is interactable
             }
             else
             {
-                btn.interactable = false;
+                btn.interactable = false;                                             //otherwise, it is not interactable
             }
         }
 
-        private void Effectiveness()
+        private void Effectiveness()                                                  //function that reduces the effectiveness of the antibiotic ability after every use
         {
             if (counter == 3)
             {
-                healthManager.currentHealth += 25;
-                counter -= 1;
+                healthManager.currentHealth += 25;                                    //if counter is 3, adds health by 25
+                counter -= 1;                                                         //reduces counter by 1
             }
 
             else if (counter == 2)
             {
-                healthManager.currentHealth += 15;
-                counter -= 1;
+                healthManager.currentHealth += 15;                                    //if counter is 2, adds health by 15
+                counter -= 1;                                                         //reduces counter by 1
             }
 
             else if (counter == 1)
             {
-                healthManager.currentHealth += 10;
-                counter -= 1;
+                healthManager.currentHealth += 10;                                    //if counter is 1, adds health by 10
+                counter -= 1;                                                         //reduces counter by 1, now counter is 0 and the ability can never be used again
             }
         }
 
